9e882a6ee68acbd4fb980c01ff83b47117cfcf59
white-zero <419162177@qq.com>
2021-12-06 09:19:53 +0800

    add package--react-lottie

--- shop/src/pages/Demo/index.tsx
+++ shop/src/pages/Demo/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
-const  Demo: React.FC<any> = () => {
 
-  
+const Demo: React.FC<any> = () => {
+
+
 
   return (
     <div>
       qqq
+      <Player
+        autoplay={true}
+        loop={false}
+        src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
+        style={{ height: '300px', width: '300px' }}
+        keepLastFrame={true}
+        onEvent={(value)=>{
+          console.log(value,'----');
+        }}
+      >
+        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+      </Player>
     </div>
   );
 }
